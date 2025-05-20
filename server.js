d0c77945151ac5827a3d66e9340639bbe1cb6f9d
rawera_daniel <rawera.daniel@gmail.com>
2025-05-20 18:58:54 +0300

    feat: add HTTP DELETE endpoint /api/v1/dictionaries/:id to delete dictionary

--- server.js
+++ server.js
@@ -55,6 +55,20 @@ app.patch("/api/v1/dictionaries/:id", (req, res) => {
   });
 });
 
+app.delete("/api/v1/dictionaries/:id", (req, res) => {
+  if (req.params.id * 1 > dictionaries.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
