a606bda54f222051543b85d3684eadb0114dca37
rawera_daniel <rawera.daniel@gmail.com>
2025-06-03 15:38:37 +0300

    refactor: move server-related files into server directory

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+const dictionaryRouter = require("./routes/dictionaryRoutes");
+const userRouter = require("./routes/userRoutes");
+
+// Middleware to parse JSON
+app.use(express.json());
+
+app.use("/api/v1/dictionaries", dictionaryRouter);
+app.use("/api/v1/users", userRouter);
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,15 +1,5 @@
-const express = require("express");
-const app = express();
 const PORT = 8000;
-
-const dictionaryRouter = require("./routes/dictionaryRoutes");
-const userRouter = require("./routes/userRoutes");
-
-// Middleware to parse JSON
-app.use(express.json());
-
-app.use("/api/v1/dictionaries", dictionaryRouter);
-app.use("/api/v1/users", userRouter);
+const app = require("./app");
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
